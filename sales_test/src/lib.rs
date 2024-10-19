b3d76daad1d384bec9fede5f67983e9d6b2c0cc5
James Holman <jameswarringtonholman@gmail.com>
2024-10-19 14:18:06 +1100

    test: make test more readable

--- sales_test/src/lib.rs
+++ sales_test/src/lib.rs
@@ -1,2 +1,2 @@
 #[cfg(test)]
-pub mod tests;
+pub mod test_create_order;
---
+++ sales_test/src/test_create_order.rs
@@ -0,0 +1,46 @@
+use axum::{
+    body::Body,
+    http::{Request, StatusCode},
+};
+use common::{Message, MessageBus};
+use std::sync::Arc;
+use tower::ServiceExt;
+
+#[tokio::test]
+async fn test_create_order() {
+    // Setup
+    let message_bus = Arc::new(MessageBus::new());
+    let app = sales::endpoints::init_router(message_bus.clone());
+    let request = Request::builder()
+        .method("POST")
+        .uri("/sales")
+        .body(Body::empty())
+        .unwrap();
+
+    // Act
+    let response = app.oneshot(request).await.unwrap();
+
+    let response_status = response.status();
+    let body = axum::body::to_bytes(response.into_body(), 2000)
+        .await
+        .unwrap();
+    let body_str = String::from_utf8(body.to_vec()).unwrap();
+    let message = message_bus
+        .receiver
+        .recv()
+        .await
+        .expect("No message was received on the message bus");
+
+    // Assert
+    assert_eq!(response_status, StatusCode::OK);
+    assert!(body_str.contains("Order"));
+    assert!(
+        matches!(message, Message::OrderPlaced(_)),
+        "Received unexpected message type"
+    );
+    match message {
+        Message::OrderPlaced(order_placed) => {
+            assert!(body_str.contains(&order_placed.order_id.to_string()));
+        }
+    }
+}
